84d34d4c9bd2a2127b166a7c879fd008b48bab68
Seren <cmingxuan94@gmail.com>
2018-05-18 22:32:59 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Command = require('command')
+module.exports = function elinNpc(dispatch) {
+	const config = require('./config.json'),
+		command = Command(dispatch)
+		
+	let {shapeId,enabled} = config,
+		length = shapeId.length
+	
+	command.add('elinnpc', () => {
+		enabled = !enabled
+		command.message(`(Elin Npc) ${enabled ? 'Enabled' : 'Disabled'}`)
+	})
+	
+	dispatch.hook('S_SPAWN_NPC', 8 , event => {
+		if(!enabled) return
+		if(event.villager) {
+			event.unk1 = shapeId[Math.floor(Math.random() * length)]
+			return true
+		}
+	})
+}
