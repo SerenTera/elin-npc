1606d446a2074f567e7db99df3d3b59954d6ae8f
Seren <cmingxuan94@gmail.com>
2018-05-19 19:12:36 +0800

    Handle Dungeons Npc

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ module.exports = function elinNpc(dispatch) {
 		command = Command(dispatch)
 		
 	let {shapeId,enabled} = config,
-		length = shapeId.length
+		length = shapeId.length,
+		inDungeon=false
 	
 	command.add('elinnpc', () => {
 		enabled = !enabled
 		command.message(`(Elin Npc) ${enabled ? 'Enabled' : 'Disabled'}`)
 	})
 	
+	dispatch.hook('S_LOAD_TOPO', 3, event => {
+		inDungeon = event.zone > 9000
+	})
+	
 	dispatch.hook('S_SPAWN_NPC', 8 , event => {
-		if(!enabled) return
+		if(!enabled || inDungeon) return
 		if(event.villager && event.owner == 0) {
 			event.unk1 = shapeId[Math.floor(Math.random() * length)]
 			return true
