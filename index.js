b373020be0a4ba965f21ec9bb68efd61d7122faa
Seren <cmingxuan94@gmail.com>
2018-08-22 17:00:48 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const Command = require('command')
-module.exports = function elinNpc(dispatch) {
-	const config = require('./config.json'),
-		command = Command(dispatch)
+module.exports = function elinNpc(mod) {
+	const config = require('./config.json')
 		
 	let {shapeId,enabled} = config,
 		length = shapeId.length,
 		inDungeon=false
 	
-	command.add('elinnpc', () => {
-		enabled = !enabled
-		command.message(`(Elin Npc) ${enabled ? 'Enabled' : 'Disabled'}`)
+	mod.command.add('elinnpc', {
+		$none() {
+			enabled = !enabled
+			mod.command.message(`(Elin Npc) ${enabled ? 'Enabled' : 'Disabled'}`)
+		}
 	})
 	
-	dispatch.hook('S_LOAD_TOPO', 3, event => {
+	mod.hook('S_LOAD_TOPO', 3, event => {
 		inDungeon = event.zone > 9000
 	})
 	
-	dispatch.hook('S_SPAWN_NPC', 9 , event => {
+	mod.hook('S_SPAWN_NPC', 9 , event => {
 		if(!enabled || inDungeon) return
 		if(event.villager && event.owner == 0) {
 			event.shapeId = shapeId[Math.floor(Math.random() * length)]
